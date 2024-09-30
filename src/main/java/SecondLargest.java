ef438dc033433747b166fcaac0f7b3533e4daac4
Sumaiya Tabassum <83543756+ST6977@users.noreply.github.com>
2024-09-30 05:38:46 -0700

    Update SecondLargest.java

--- src/main/java/SecondLargest.java
+++ src/main/java/SecondLargest.java
@@ -2,19 +2,19 @@ public class SecondLargest {
     public static void main(String[]args){
          double [] cgpas = {3.50, 3.52, 3.43, 3.63, 3.48, 3.32, 3.30, 3.60, 3.86, 3.75};
          double max = cgpas[0];
-        double sLargest = cgpas[0];
+        double sHeighest = cgpas[0];
 
         //loop
         for (int i = 0;i< cgpas.length; i++){
             if(cgpas[i] > max){
-                sLargest = max; //swapping
+              sHeighest = max; //swapping
                 max  = cgpas[i];
             }
-            else if (cgpas[i] >   sLargest){
-                sLargest = cgpas[i];
+            else if (cgpas[i] >  sHeighest){
+               sHeighest = cgpas[i];
             }
         }
-        System.out.println(sLargest);
+        System.out.println(sHeighest);
     }
 
 }
